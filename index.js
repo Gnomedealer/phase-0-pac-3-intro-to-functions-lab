794e097d886625095e07162d897080fe165bac25
Tommy Nguyen <sixty22tommy@gmail.com>
2022-06-13 02:02:26 -0700

    commit tree 88ab35a3e6ddf1cb153d60e8505a8e4598c5804b

---
+++ index.js
@@ -0,0 +1,22 @@
+function shout(string) {
+    return string.toUpperCase();
+  }
+  "Hello!".toUpperCase();
+function whisper(string) {
+    return string.toLowerCase();
+  }
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+function logWhisper(string){
+    console.log(string.toLowerCase())
+}
+function sayHiToHeadphonedRoommate(string){
+    return string.toLowerCase()
+    return string.toUpperCase()
+    return string.toEqual()
+}
+'hello'.toEqual() 
+'I can\'t hear you!'.toLowerCase()
+"YES INDEED!".toUpperCase()
+"Let's have dinner together!".toEqual()
